8817bd656fdf0fee19a34e6605bff2c84607002e
gpetrou <4172445+gpetrou@users.noreply.github.com>
2023-03-22 19:12:39 +0200

    Add naming analyzer for boolean members (#17)

---
+++ src/GP.NamingAnalyzers/DiagnosticAnalyzers/BooleanMemberNameDiagnosticAnalyzer.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (c) gpetrou. All Rights Reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace GP.NamingAnalyzers.DiagnosticAnalyzers;
+
+/// <summary>
+/// An analyzer to validate boolean names.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class BooleanMemberNameDiagnosticAnalyzer : DiagnosticAnalyzer
+{
+    private const string DiagnosticId = "GPNA0004";
+    private const string Title = "Incorrect boolean name";
+    private const string MessageFormat = "Boolean '{0}' does not follow the 'can|has|is' naming convention";
+    private const string Description = "A boolean name should follow the 'can|has|is' naming convention.";
+    private const string Category = "Naming";
+    private const string HelpLinkUri = $"https://github.com/gpetrou/GP.NamingAnalyzers/tree/main/docs/{DiagnosticId}.md";
+
+    private static readonly DiagnosticDescriptor DiagnosticDescriptor = new(
+        DiagnosticId,
+        Title,
+        MessageFormat,
+        Category,
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        Description,
+        HelpLinkUri);
+
+    private static readonly Regex BooleanNameRegex = new("^_?(((C|c)an)|((H|h)as)|((I|i)s))[A-Z][a-zA-Z0-9]*$", RegexOptions.Compiled);
+
+    /// <inheritdoc/>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(DiagnosticDescriptor);
+
+    /// <summary>
+    /// Returns a value indicating whether the provided name is a valid boolean name.
+    /// </summary>
+    /// <param name="name">The name to validate.</param>
+    /// <returns><see langword="true"/> if the provided name is a valid boolean name; otherwise, <see langword="false"/>.</returns>
+    public static bool IsBooleanNameValid(string name) =>
+        !string.IsNullOrWhiteSpace(name) &&
+        BooleanNameRegex.IsMatch(name);
+
+    /// <summary>
+    /// Analyzes a symbol.
+    /// </summary>
+    /// <param name="context">The symbol analysis context.</param>
+    /// <param name="booleanSymbol">The boolean symbol.</param>
+    private static void AnalyzeSymbol(SymbolAnalysisContext context, INamedTypeSymbol booleanSymbol)
+    {
+        ISymbol symbol = context.Symbol;
+        if (symbol is IFieldSymbol fieldSymbol)
+        {
+            if (fieldSymbol.Type.OriginalDefinition.Equals(booleanSymbol, SymbolEqualityComparer.Default) && !IsBooleanNameValid(fieldSymbol.Name))
+            {
+                Diagnostic diagnostic = Diagnostic.Create(DiagnosticDescriptor, symbol.Locations[0], fieldSymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+
+                return;
+            }
+        }
+
+        if (symbol is IPropertySymbol propertySymbol)
+        {
+            if (propertySymbol.Type.OriginalDefinition.Equals(booleanSymbol, SymbolEqualityComparer.Default) && !IsBooleanNameValid(propertySymbol.Name))
+            {
+                Diagnostic diagnostic = Diagnostic.Create(DiagnosticDescriptor, symbol.Locations[0], propertySymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+
+                return;
+            }
+        }
+
+        if (symbol is IParameterSymbol parameterSymbol)
+        {
+            if (parameterSymbol.Type.OriginalDefinition.Equals(booleanSymbol, SymbolEqualityComparer.Default) && !IsBooleanNameValid(parameterSymbol.Name))
+            {
+                Diagnostic diagnostic = Diagnostic.Create(DiagnosticDescriptor, symbol.Locations[0], parameterSymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+
+                return;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Analyzes an operation.
+    /// </summary>
+    /// <param name="context">The operation analysis context.</param>
+    /// <param name="booleanSymbol">The boolean symbol.</param>
+    private static void AnalyzeOperation(OperationAnalysisContext context, INamedTypeSymbol booleanSymbol)
+    {
+        if (context.Operation is IVariableDeclarationGroupOperation variableDeclarationGroupOperation)
+        {
+            foreach (IVariableDeclarationOperation variableDeclarationOperation in variableDeclarationGroupOperation.Declarations)
+            {
+                foreach (ILocalSymbol localSymbol in variableDeclarationOperation.GetDeclaredVariables())
+                {
+                    if (localSymbol.Type.OriginalDefinition.Equals(booleanSymbol, SymbolEqualityComparer.Default) && !IsBooleanNameValid(localSymbol.Name))
+                    {
+                        Diagnostic diagnostic = Diagnostic.Create(DiagnosticDescriptor, localSymbol.Locations[0], localSymbol.Name);
+                        context.ReportDiagnostic(diagnostic);
+
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public override void Initialize(AnalysisContext context)
+    {
+        if (context is null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+        {
+            if (compilationStartAnalysisContext is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            INamedTypeSymbol? booleanSymbol = compilationStartAnalysisContext.Compilation.GetTypeByMetadataName(typeof(bool).FullName);
+
+            if (booleanSymbol is not null)
+            {
+                compilationStartAnalysisContext.RegisterSymbolAction(
+                    symbolAnalysisContext => AnalyzeSymbol(symbolAnalysisContext, booleanSymbol),
+                    SymbolKind.Field,
+                    SymbolKind.Property,
+                    SymbolKind.Parameter);
+                compilationStartAnalysisContext.RegisterOperationAction(
+                    operationAnalysisContext => AnalyzeOperation(operationAnalysisContext, booleanSymbol),
+                    OperationKind.VariableDeclarationGroup);
+            }
+        });
+    }
+}
---
+++ tests/GP.NamingAnalyzers.Test/DiagnosticAnalyzers/BooleanMemberNameDiagnosticAnalyzerTests.cs
@@ -0,0 +1,190 @@
+﻿// Copyright (c) gpetrou. All Rights Reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using FluentAssertions;
+using GP.NamingAnalyzers.DiagnosticAnalyzers;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Testing;
+using Xunit;
+using VerifyCS = GP.NamingAnalyzers.Test.Verifiers.CSharpAnalyzerVerifier<GP.NamingAnalyzers.DiagnosticAnalyzers.BooleanMemberNameDiagnosticAnalyzer>;
+
+namespace GP.NamingAnalyzers.Test.DiagnosticAnalyzers;
+
+public sealed class BooleanMemberNameDiagnosticAnalyzerTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("_myBoolean")]
+    public void IsBooleanNameValid_WhenNameIsInvalid_ShouldReturnFalse(string booleanName)
+    {
+        bool isNameValid = BooleanMemberNameDiagnosticAnalyzer.IsBooleanNameValid(booleanName);
+
+        isNameValid.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("canBeEdited")]
+    [InlineData("hasBeenEdited")]
+    [InlineData("isEditing")]
+    public void IsBooleanNameValid_WhenNameIsValid_ShouldReturnTrue(string booleanName)
+    {
+        bool isNameValid = BooleanMemberNameDiagnosticAnalyzer.IsBooleanNameValid(booleanName);
+
+        isNameValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task Analyze_WhenCodeIsEmpty_ShouldNotReportADiagnostic()
+    {
+        const string sourceCode = @"";
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode);
+    }
+
+    [Fact]
+    public async Task Analyze_WhenMembersAreNotBoolean_ShouldNotReportADiagnostic()
+    {
+        string sourceCode = @"
+using System;
+using System.Collections.Generic;
+
+namespace N
+{
+    public class Example
+    {
+        private int IntegerField;
+        private string StringProperty { get; set; }
+
+        public float Check(List<double> listParameter)
+        {
+            long[] values;
+
+            return 1.0f;
+        }
+    }
+}";
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode);
+    }
+
+    [Fact]
+    public async Task Analyze_WhenBooleanFieldNameIsInvalid_ShouldReportDiagnostic()
+    {
+        string sourceCode = $@"
+using System;
+
+namespace N
+{{
+    public class Example
+    {{
+        private bool _myBoolean;
+        private bool _isValid;
+    }}
+}}";
+
+        DiagnosticResult[] expectedDiagnosticResults = new DiagnosticResult[]
+        {
+            new DiagnosticResult("GPNA0004", DiagnosticSeverity.Warning)
+                .WithMessage("Boolean '_myBoolean' does not follow the 'can|has|is' naming convention")
+                .WithSpan(8, 22, 8, 32)
+                .WithArguments("_myBoolean")
+        };
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode, expectedDiagnosticResults);
+    }
+
+    [Fact]
+    public async Task Analyze_WhenBooleanPropertyNameIsInvalid_ShouldReportDiagnostic()
+    {
+        string sourceCode = $@"
+using System;
+using System.Collections.Generic;
+
+namespace N
+{{
+    public class Example
+    {{
+        public bool MyBoolean {{ get; set; }}
+        public bool IsValid {{ get; set; }}
+    }}
+}}";
+
+        DiagnosticResult[] expectedDiagnosticResults = new DiagnosticResult[]
+        {
+            new DiagnosticResult("GPNA0004", DiagnosticSeverity.Warning)
+                .WithMessage("Boolean 'MyBoolean' does not follow the 'can|has|is' naming convention")
+                .WithSpan(9, 21, 9, 30)
+                .WithArguments("MyBoolean")
+        };
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode, expectedDiagnosticResults);
+    }
+
+    [Fact]
+    public async Task Analyze_WhenBooleanParameterNameIsInvalid_ShouldReportDiagnostic()
+    {
+        string sourceCode = $@"
+using System;
+using System.Collections.Generic;
+
+namespace N
+{{
+    public class Example
+    {{
+        public void Check(bool myBoolean)
+        {{
+        }}
+
+        public void CheckOnceMore(bool isValid)
+        {{
+        }}
+    }}
+}}";
+
+        DiagnosticResult[] expectedDiagnosticResults = new DiagnosticResult[]
+        {
+            new DiagnosticResult("GPNA0004", DiagnosticSeverity.Warning)
+                .WithMessage("Boolean 'myBoolean' does not follow the 'can|has|is' naming convention")
+                .WithSpan(9, 32, 9, 41)
+                .WithArguments("myBoolean")
+        };
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode, expectedDiagnosticResults);
+    }
+
+    [Fact]
+    public async Task Analyze_WhenBooleanVariableNameIsInvalid_ShouldReportDiagnostic()
+    {
+        string sourceCode = $@"
+using System;
+using System.Collections.Generic;
+
+namespace N
+{{
+    public class Example
+    {{
+        public void Check()
+        {{
+            bool myBoolean;
+        }}
+
+        public void CheckOnceMore()
+        {{
+            bool isValid;
+        }}
+    }}
+}}";
+
+        DiagnosticResult[] expectedDiagnosticResults = new DiagnosticResult[]
+        {
+            new DiagnosticResult("GPNA0004", DiagnosticSeverity.Warning)
+                .WithMessage("Boolean 'myBoolean' does not follow the 'can|has|is' naming convention")
+                .WithSpan(11, 18, 11, 27)
+                .WithArguments("myBoolean")
+        };
+
+        await VerifyCS.VerifyAnalyzerAsync(sourceCode, expectedDiagnosticResults);
+    }
+}
