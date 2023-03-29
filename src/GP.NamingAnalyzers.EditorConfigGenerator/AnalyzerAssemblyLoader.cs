d4471547792cb496c0b7db92c36423d5ae56d541
gpetrou <gipetrou@gmail.com>
2023-03-29 23:18:54 +0300

    Add editorconfig generator

---
+++ src/GP.NamingAnalyzers.EditorConfigGenerator/AnalyzerAssemblyLoader.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) gpetrou. All Rights Reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+
+namespace GP.NamingAnalyzers.EditorConfigGenerator;
+
+/// <summary>
+/// Handles loading analyzer assemblies and their dependencies.
+/// </summary>
+public sealed class AnalyzerAssemblyLoader : IAnalyzerAssemblyLoader
+{
+    /// <summary>
+    /// The default instance of <see cref="AnalyzerAssemblyLoader"/>.
+    /// </summary>
+    public static readonly IAnalyzerAssemblyLoader Instance = new AnalyzerAssemblyLoader();
+
+    private AnalyzerAssemblyLoader()
+    {
+    }
+
+    /// <inheritdoc/>
+    public void AddDependencyLocation(string fullPath)
+    {
+    }
+
+    /// <inheritdoc/>
+    public Assembly LoadFromPath(string fullPath) =>
+        Assembly.LoadFrom(fullPath);
+}
---
+++ src/GP.NamingAnalyzers.EditorConfigGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) gpetrou. All Rights Reserved.
+// Licensed under the MIT License. See License.md in the project root for license information.
+
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
